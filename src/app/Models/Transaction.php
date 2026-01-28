4c5db8f1096c5a6174a8c09428f1cf8857bc3369
kaorivujic <kiyomaru.56129@gmail.com>
2026-01-29 03:02:39 +0900

    create models

--- src/app/Models/Transaction.php
+++ src/app/Models/Transaction.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Vendor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'transaction_date',
+        'category_id',
+        'amount',
+        'payment_method',
+        'vendor_id',
+        'note'
+    ];
+
+    protected $casts = [
+        'transaction_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
